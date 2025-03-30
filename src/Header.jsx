a005298dce27df1b1131133ac33e989ad3d04781
paulthiru122 <thiruprogrammer@gmail.com>
2025-03-30 12:48:27 +0530

    building the header options completed

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,6 +4,11 @@ import SearchIcon from '@mui/icons-material/Search';
 import  HeaderOption  from './HeaderOption';
 import HomeIcon from '@mui/icons-material/Home';
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount'; 
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import Avatar from '@mui/material/Avatar';
+
 
 export default function Header() {
   
@@ -22,6 +27,10 @@ export default function Header() {
      <div className="header-right">
       <HeaderOption Icon={HomeIcon} title='Home' />
       <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
+      <HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
+      <HeaderOption Icon={ChatIcon} title='Messaging' />
+      <HeaderOption Icon={NotificationsIcon} title='Notifications' />
+      <HeaderOption Avatar = {Avatar} title= 'me' />
      </div>
 
     </div>
--- src/HeaderOption.jsx
+++ src/HeaderOption.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './HeaderOption.css';
 
-export const HeaderOption = ({Icon,title}) => {
+export const HeaderOption = ({Icon,Avatar,title}) => {
   return (
     <div className='headeroption'>
         {Icon && <Icon className='headeroption-icon'/>}
+        {Avatar &&
+        <Avatar className='headeroption-icon' src={Avatar} />}
         <h3 className='headeroption-title'>{title}</h3>
 
     </div>
